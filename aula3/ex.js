0cb8f7cf2f59a7064546411c3031a778bcf8bb90
filipicaliman <fgcaliman.dev@gmail.com>
2022-08-25 15:49:30 -0300

    att

---
+++ aula3/ex.js
@@ -0,0 +1,2 @@
+console.log('Meu nome é "Filipi". Estou aprendendo JavaScript ás', 15, 'da tarde');
+console.log()
---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+console.log("Filipi Caliman"); //string
+console.log('Filipi Caliman');
+console.log(`filipi`)
+
+console.log(123456, 15.85, 'Filipi')

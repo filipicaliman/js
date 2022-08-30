7dc4be15acd329c4547d09287c63334eba29c748
filipicaliman <fgcaliman.dev@gmail.com>
2022-08-30 17:15:02 -0300

    att js

---
+++ aula14/index.js
@@ -0,0 +1,50 @@
+// IEEE 754-2008
+
+//let num1 = 10.5646346363;
+//let num2 = 2.5
+
+// console.log(num1.toString() + num2);  //transformando em uma string
+//num1 = num1.toString();
+
+//console.log(num1.toString(2)); //numeros binários
+
+/*let num1 = 10.5646346363;
+let num2 = 2.5
+
+console.log(num1.toFixed(3));  //numero de casas decimais
+*/
+
+/*
+let num1 = 10;              
+let num2 = 2.5
+console.log(Number.isInteger(num1));        //numero inteiro ou não
+*/
+
+/*
+let temper = num1 * 'ola';
+console.log(Number.isNaN(temp)); //conta não é executavel? V ou F
+*/
+
+/*
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2; //0.8
+num1 += num2; //0.9
+num1 += num2; //1.0
+num1 += num2; //1.1
+num1 += num2; //1.2
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2; //1.9
+num1 += num2; //2.0
+
+num1 = Number(num1.toFixed(2));  //numero real que aparece
+
+console.log(num1);
+console.log(Number.isInteger(num1)); //num inteiro?;
+*/
---
+++ aula15/index.js
@@ -0,0 +1,14 @@
+let num1 = 4.39884;
+// let num2 = Math.floor(num1); //arredonda para baixo
+
+// let num2 = Math.ceil(num1); // arredonda p cima
+
+//let num2 = Math.round(num1) // arredonda p mais proximo
+//console.log(num2)
+
+//console.log(Math.max(9,1,-5,3,8)); //maior numero 
+//console.log(Math.min(9,1,-5,3,8)); //menor numero
+
+                  //num inteiro
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5); 
+//console.log(aleatorio)

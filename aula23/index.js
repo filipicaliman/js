9bf1dcef62cb331e47e95d95439a0bb148ed2954
filipicaliman <fgcaliman.dev@gmail.com>
2022-09-08 16:41:35 -0300

    js

--- aula23/index.js
+++ aula23/index.js
@@ -1,1 +1,16 @@
+// function falaOi () {         //vai retornar o valor falso
+//     return 'Oi';
+// }
 
+// const vaiExecutar = 'Joãozinho';
+
+// console.log(vaiExecutar && falaOi ()); // and
+
+// vai retornar o primeiro valor verdadeiro
+
+// console.log(false || NaN || '' || 'joao' )
+
+// const corUsuario = 'vermelho';
+// const corPadrao = corUsuario || 'preto';    //se o usuario escolher ele tem prioridade, se n escolher é preto.
+
+// console.log(corPadrao)

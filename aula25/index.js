998a3f9d78a15a6043250a0db98108f91cade8ab
filipicaliman <fgcaliman.dev@gmail.com>
2022-09-12 15:21:48 -0300

    js

---
+++ aula25/index.js
@@ -0,0 +1,9 @@
+const num = 10;
+
+if (num >= 0 && num <=5) {
+    console.log('O número está entre 0 e 5.');
+} else if (num <= 10) {
+    console.log('O número está entre 6 e 10.');
+} else {
+    console.log('O número não esta entre 0 e 10.');
+}
